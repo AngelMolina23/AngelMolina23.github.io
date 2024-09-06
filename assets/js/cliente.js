34a916c3cbdfa750b7726cf98ee3242f2e89ee26
AngelMolina23 <125847601+AngelMolina23@users.noreply.github.com>
2024-09-06 14:14:16 -0600

    segundo commit

---
+++ assets/js/cliente.js
@@ -0,0 +1,16 @@
+function saludar (){
+    console.log('Diseño web 1')
+    //alert('Diseño web 1')
+    console.log(document);
+    console.log(document.getElementById('parrafo1').innerHTML);
+    console.log(document.getElementById('parrafo1').getAttribute('style'));
+}
+function cambiarParrafo2(){
+document.getElementById('parrafo2').innerHTML="FES ARAGON UNAM"
+}
+function cambiarFondoBody(){
+    const colores = ['#FFFFFF','#33FF57','#2222FF','#22FF11','#FF2222'];
+    const colorAlea = colores[Math.floor(Math.random()*5)];
+    console.log(colorAlea);
+    document.getElementById('cuerpo').style.backgroundColor = colorAlea;
+}
